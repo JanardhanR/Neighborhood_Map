6bd6267bbf99ae8bd1589d9a387a1de64d453793
JanardhanR <jana.dhana@outlook.com>
2017-12-28 21:28:08 +0530

    Markers without proper zoom

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 var map;
+var largeInfowindow;
+
 var markers = [];
 
 var nammaLocations = [
-    { title: 'Nagasandra', location: { lat: 13.0432, lng: 77.5003 } },
+    // { title: 'Nagasandra', location: { lat: 13.0432, lng: 77.5003 } },
     { title: 'Baiyappanahalli', location: { lat: 12.9913, lng: 77.6521 } },
     { title: 'Swami Vivekananda Road', location: { lat: 12.9859, lng: 77.6449 } },
     { title: 'Indiranagar', location: { lat: 12.9719, lng: 77.6412 } },
@@ -11,7 +13,11 @@ var nammaLocations = [
     { title: 'Cubbon Park', location: { lat: 12.9763, lng: 77.5929 } },
     { title: 'Vidhana Souda', location: { lat: 12.9795, lng: 77.5909 } },
     { title: 'Sir M.Visveshwaraiah', location: { lat: 12.974615, lng: 77.584785 } },
-    { title: 'Majestic', location: { lat: 12.975692, lng: 77.572837 } }
+    { title: 'Majestic', location: { lat: 12.975692, lng: 77.572837 } },
+    //
+    { title: 'Dummy 1', location: { lat: 12.9728, lng: 77.6317 } },
+    { title: 'Dummy 2', location: { lat: 12.9763, lng: 77.5949 } },
+    { title: 'Dummy 3', location: { lat: 12.9795, lng: 77.5809 } }
 ];
 
 var customstyles = [
@@ -97,7 +103,6 @@ var customstyles = [
 
 // This function will loop through the markers array and display them all.
 function showListings() {
-    console.log("Reached show listings");
     var bounds = new google.maps.LatLngBounds();
     // Extend the boundaries of the map for each marker and display the marker
     for (var i = 0; i < markers.length; i++) {
@@ -105,6 +110,27 @@ function showListings() {
         bounds.extend(markers[i].position);
     }
     map.fitBounds(bounds);
+}
+
+function showMarker(location) {
+    var bounds = new google.maps.LatLngBounds();
+    for (var i = 0; i < markers.length; i++) {
+        console.log("printing showmarker titles");
+        console.log(location.toLowerCase());
+        console.log(markers[i].title.toLowerCase());
+        if ((markers[i].title.toLowerCase().indexOf(location.toLowerCase()) !== -1)) {
+            //if ((markers[i].position.lat() == location.lat) && (markers[i].position.lng() == location.lng)) {            
+            markers[i].setMap(map);
+            bounds.extend(markers[i].position);
+        }
+    }
+    map.fitBounds(bounds);
+}
+
+function hideMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+    }
 }
 
 // This function takes in a COLOR, and then creates a new marker
@@ -118,13 +144,31 @@ function makeMarkerIcon(markerColor) {
         new google.maps.Point(0, 0),
         new google.maps.Point(10, 34),
         new google.maps.Size(21, 34));
+    console.log("marker : " + markerImage);
     return markerImage;
 }
 
 var MetroLocation = function (data) {
     var self = this;
     this.title = ko.observable(data.title);
+    this.location = ko.observable(data.location);
 };
+
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick', function () {
+            infowindow.marker = null;
+        });
+    }
+}
 
 function initMap() {
     var self = this;
@@ -132,15 +176,16 @@ function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 13.072651, lng: 77.796172 },
         zoom: 13,
-        styles:customstyles
+        styles: customstyles
     });
     // Style the markers a bit. This will be our listing marker icon.
     var defaultIcon = makeMarkerIcon('6a28e6');
 
     // Create a "highlighted location" marker color for when the user
     // mouses over the marker.
-    var highlightedIcon = makeMarkerIcon('380707');
+    var highlightedIcon = makeMarkerIcon('74ee0f');
 
+    largeInfowindow = new google.maps.InfoWindow();
     // The following group uses the location array to create an array of markers on initialize.
     for (var i = 0; i < nammaLocations.length; i++) {
         // Get the position from the location array.
@@ -183,9 +228,12 @@ var ViewModel = function () {
 
     this.doFilter = function () {
         self.filterLocations.removeAll();
+        hideMarkers();
         nammaLocations.forEach(function (locItem) {
             if (locItem.title.toLowerCase().indexOf(self.filterText().toLowerCase()) != -1) {
                 self.filterLocations.push(new MetroLocation(locItem));
+                // console.log("Entering showmarker - lat,lng : " + locItem.location.lat + "," + locItem.location.lng);
+                showMarker(locItem.title);
             }
         });
     };
@@ -193,6 +241,14 @@ var ViewModel = function () {
         self.doFilter();
     });
     self.googlemap = map;
+
+    this.onClick = function ($index, data, event) {
+        var indexTouse = $index();
+        var metLoc = (ko.observable(self.filterLocations()[indexTouse]));
+        console.log(metLoc.title);
+        //need to pass the marker, passign MetroLocation object wrongly
+        //populateInfoWindow(metLoc, largeInfowindow);
+    };
 };
 
 function loadMap() {
